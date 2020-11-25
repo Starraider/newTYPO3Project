b0491bd19fbcfb479985916c5693eca2dec89351
Sven K <sven@skom.de>
2020-11-25 14:13:39 +0530

    feat: First commit

---
+++ README.md
@@ -0,0 +1,79 @@
+[![Build Status](https://travis-ci.org/benjaminkott/site-introduction.svg?branch=master)](https://travis-ci.org/benjaminkott/site-introduction)
+
+# DDEV Setup
+
+To use this repository we recommend to use ddev as environment. Please download
+and install DDEV https://github.com/drud/ddev/releases.
+
+[LINUX] Change permissions of ./var to 0777 (`chmod 0777 ./var/cache`) on host
+
+* `ddev start`
+* `ddev import-db --src=./data/db.sql`
+* `ddev import-files --src=./assets`
+* `ddev composer install`
+
+# Frontend
+
+* TYPO3: http://introduction.ddev.site
+* Mail Hogg: http://introduction.ddev.site:8025
+* PHP My Admin: http://introduction.ddev.site:8036
+
+# Credentials Backend
+
+* URL: http://introduction.ddev.local/typo3
+* Username: `admin`
+* Password: `password`
+
+# Admin Tools
+
+* URL: http://introduction.ddev.site/typo3/install.php
+* Password: `password`
+
+# Executing Commands
+
+If you need to execute commands like `composer` or `bin/typo3` you need to run
+these commands within the ddev containers. You can easily log into the web
+container by executing the command `ddev ssh`. Its also possible to run commands
+within the container without the need to log into it.
+
+* Composer Install: `ddev composer install`
+* Database Export: `ddev exec bin/typo3 ddev:exportdb`
+
+# Execute acceptance tests
+
+The ddev setup comes with a selenium-chrome container, codeception and some
+acceptance tests ready to run.
+
+* Run tests: `ddev composer test`
+
+# Environment Variables
+
+This setup is preconfigured to work with ddev. If you plan to use this setup
+in a different context, please create a `.env` file and adapt the settings
+to your system.
+
+## .env.dist
+
+```
+# Database Credentials
+TYPO3_DB_CONNECTIONS_DEFAULT_HOST = "db"
+TYPO3_DB_CONNECTIONS_DEFAULT_PORT = 3306
+TYPO3_DB_CONNECTIONS_DEFAULT_USER = "db"
+TYPO3_DB_CONNECTIONS_DEFAULT_PASS = "db"
+TYPO3_DB_CONNECTIONS_DEFAULT_NAME = "db"
+
+# Graphics
+TYPO3_GFX_PROCESSOR = "ImageMagick"
+TYPO3_GFX_PROCESSOR_PATH = "/usr/bin/"
+TYPO3_GFX_PROCESSOR_PATH_LZW = "/usr/bin/"
+
+# Mail
+TYPO3_MAIL_TRANSPORT = "smtp"
+TYPO3_MAIL_TRANSPORT_SMTP_SERVER = "localhost:1025"
+
+# System
+TYPO3_TRUSTED_HOST_PATTERN = "introduction.ddev.site"
+
+# Site
+SITE_INTRODUCTION_BASE = "http://introduction.ddev.site/"
+```
---
+++ public/typo3conf/LocalConfiguration.php
@@ -0,0 +1,117 @@
+<?php
+return [
+    'BE' => [
+        'debug' => true,
+        'explicitADmode' => 'explicitAllow',
+        'installToolPassword' => '$2y$12$V8Xxf88bKuyJ6xlNnGQbKuBer/rDnBig8FknlCfSbSUchD8q686Pu',
+        'loginSecurityLevel' => 'normal',
+        'passwordHashing' => [
+            'className' => 'TYPO3\\CMS\\Core\\Crypto\\PasswordHashing\\BcryptPasswordHash',
+            'options' => [],
+        ],
+    ],
+    'DB' => [
+        'Connections' => [
+            'Default' => [
+                'charset' => 'utf8',
+                'driver' => 'mysqli',
+            ],
+        ],
+    ],
+    'EXT' => [],
+    'EXTCONF' => [
+        'lang' => [
+            'availableLanguages' => [
+                'de',
+            ],
+        ],
+    ],
+    'EXTENSIONS' => [
+        'backend' => [
+            'backendFavicon' => '',
+            'backendLogo' => 'EXT:bootstrap_package/Resources/Public/Images/Backend/backend-logo.svg',
+            'loginBackgroundImage' => 'EXT:bootstrap_package/Resources/Public/Images/Backend/login-background-image.jpg',
+            'loginFootnote' => '',
+            'loginHighlightColor' => '',
+            'loginLogo' => 'EXT:bootstrap_package/Resources/Public/Images/Backend/login-logo.svg',
+        ],
+        'bootstrap_package' => [
+            'disableCssProcessing' => '0',
+            'disableFontLoader' => '0',
+            'disableGoogleFontCaching' => '0',
+            'disablePageTsBackendLayouts' => '0',
+            'disablePageTsContentElements' => '0',
+            'disablePageTsRTE' => '0',
+            'disablePageTsTCEFORM' => '0',
+            'disablePageTsTCEMAIN' => '0',
+        ],
+        'extensionmanager' => [
+            'automaticInstallation' => '1',
+            'offlineMode' => '0',
+        ],
+        'indexed_search' => [
+            'catdoc' => '/usr/bin/',
+            'debugMode' => '0',
+            'disableFrontendIndexing' => '0',
+            'enableMetaphoneSearch' => '1',
+            'flagBitMask' => '192',
+            'fullTextDataLength' => '0',
+            'ignoreExtensions' => '',
+            'indexExternalURLs' => '0',
+            'maxAge' => '0',
+            'maxExternalFiles' => '5',
+            'minAge' => '24',
+            'pdf_mode' => '20',
+            'pdftools' => '/usr/bin/',
+            'ppthtml' => '/usr/bin/',
+            'trackIpInStatistic' => '2',
+            'unrtf' => '/usr/bin/',
+            'unzip' => '/usr/bin/',
+            'useCrawlerForExternalFiles' => '0',
+            'useMysqlFulltext' => '0',
+            'xlhtml' => '/usr/bin/',
+        ],
+        'scheduler' => [
+            'maxLifetime' => '1440',
+            'showSampleTasks' => '1',
+        ],
+    ],
+    'FE' => [
+        'debug' => true,
+        'loginSecurityLevel' => 'normal',
+        'passwordHashing' => [
+            'className' => 'TYPO3\\CMS\\Core\\Crypto\\PasswordHashing\\BcryptPasswordHash',
+            'options' => [],
+        ],
+    ],
+    'GFX' => [
+        'processor' => 'ImageMagick',
+        'processor_allowTemporaryMasksAsPng' => false,
+        'processor_colorspace' => 'sRGB',
+        'processor_effects' => true,
+        'processor_enabled' => true,
+        'processor_path' => '/usr/bin/',
+        'processor_path_lzw' => '/usr/bin/',
+    ],
+    'MAIL' => [
+        'transport' => 'sendmail',
+        'transport_sendmail_command' => '/usr/local/bin/mailhog sendmail test@example.org --smtp-addr 127.0.0.1:1025',
+        'transport_smtp_encrypt' => '',
+        'transport_smtp_password' => '',
+        'transport_smtp_server' => '',
+        'transport_smtp_username' => '',
+    ],
+    'SYS' => [
+        'devIPmask' => '*',
+        'displayErrors' => 1,
+        'encryptionKey' => '517d47fd2681627245de9c4a4a726ac2e833c8bca7c5c0ad543c12e9f2ddbd8803a83044b6a4f247b26ceddf03be451d',
+        'exceptionalErrors' => 12290,
+        'features' => [
+            'unifiedPageTranslationHandling' => true,
+        ],
+        'sitename' => 'Introduction',
+        'systemMaintainers' => [
+            1,
+        ],
+    ],
+];
